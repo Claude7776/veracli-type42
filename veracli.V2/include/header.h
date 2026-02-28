d6d6246ec43222310077651dfe9cd4cf5544b27a
Claude7776 <codecld@proton.me>
2026-02-28 01:51:41 +0100

    feat: fix AES-256 pour 32bytes GCM IV avec un standard de 12bytes NIST Slt de 16bytes GCM TAG pareil ARgon2id pour un parametrage de sécurité

--- veracli.V2/include/header.h
+++ veracli.V2/include/header.h
@@ -1,22 +1,24 @@
 #ifndef HEADER_H
-#define HEADER_H
+# define HEADER_H
 
-#include <stdint.h>
-
-#define MAGIC_SIZE 8
-#define RESERVED_SIZE 3
-
-#define KDF_ARGON2ID 0x01
+# include <stdint.h>
+# include <stdio.h>
+# include "config.h"
 
 typedef struct __attribute__((packed))
 {
-    uint8_t  magic[MAGIC_SIZE];  // "VERACLI2"
-    uint8_t  version;            // 0x01
-    uint8_t  kdf_id;             // 0x01 = Argon2id
-    uint8_t  salt_len;           // usually 16
-    uint8_t  iv_len;             // usually 12 (GCM)
-    uint8_t  tag_len;            // usually 16
-    uint8_t  reserved[RESERVED_SIZE];
-} veracli_header_t;
+    uint8_t magic[VERACLI_MAGIC_SIZE];
+    uint8_t version;
+    uint8_t kdf_id;
+    uint8_t salt_len;
+    uint8_t iv_len;
+    uint8_t tag_len;
+    uint8_t reserved[3];
+}   veracli_header_t;
+
+void header_init(veracli_header_t *header);
+int  header_validate(const veracli_header_t *header);
+int  header_write(FILE *fp, const veracli_header_t *header);
+int  header_read(FILE *fp, veracli_header_t *header);
 
 #endif
